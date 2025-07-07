a6596c739d8d092e944900a2c20ca3781eb86959
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-07-07 13:42:55 +0530

    refactor: clean up the code by move some declarations

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 //@ts-check
 
-const displaySubmissionResponse = (function () {
+(function () {
     /**@type {HTMLFormElement} */
     const form = document.getElementsByTagName("form")[0];
+    const formPage = document.querySelector(".form-page");
+    const thankYouPage = document.querySelector(".thank-you-page");
+    const ratingSpan = document.querySelector(".rating-span");
 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        const formPage = document.querySelector(".form-page");
-        const thankYouPage = document.querySelector(".thank-you-page");
-        const ratingSpan = document.querySelector(".rating-span");
-
         const formData = new FormData(form);
         const rating = formData.get("rating");
 
-        if(ratingSpan && formPage && thankYouPage) {
+        if (ratingSpan && formPage && thankYouPage) {
             ratingSpan.textContent = `${rating}`;
             formPage.classList.add("hidden");
             thankYouPage.classList.remove("hidden");
         }
     });
 })();
-
-
